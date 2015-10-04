dfe2e7b4683b6f77887ba8de72a9f8ee242c70db
jalaj <jalajiitr@gmail.com>
2015-10-04 23:11:47 +0530

    initial repo setup.

---
+++ app/scripts/controllers/testController.js
@@ -0,0 +1,13 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name sbAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sbAdminApp
+ */
+angular.module('sbAdminApp')
+  .controller('TestCtrl', function($scope) {
+  	console.log("hello baby i am here");
+    
+});
