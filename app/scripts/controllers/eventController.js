311dc7b7aafbca1a432d14b851f34fba67a63cc6
jalaj <jalajiitr@gmail.com>
2015-10-21 15:48:04 +0530

    Get events functionality added modified

--- app/scripts/controllers/eventController.js
+++ app/scripts/controllers/eventController.js
@@ -6,11 +6,18 @@ angular.module('sbAdminApp')
       $scope.eventsList= {};
       var callback = function(response)
       {
-        $scope.eventsList = response;
+      
       }
-   EventsService.GetEvents(callback);
+     EventsService.GetEvents(callback)
+     .then(function(response){
+      console.log(response.data.events);
+
+        $scope.eventsList = response.data.events;
+     })
+     console.log($scope.eventsList.length);
      $scope.bikename = "jalaj";
-   //  $scope.eventsList = JSON.parse('{"modules": { "module1": { "title": "name of module1","description": "description of module1", "weeks": {"week1": {"title": "Week 01"}}},"module2": {"title": "name of module2", "description": "description of module2", "weeks": { "week2": {"title": "Week 02"},"week3": { "title": "Week 03" } } } }}');
-console.log($scope.eventsList);     
+     //$scope.eventsList = JSON.parse('{"modules": { "module1": { "title": "name of module1","description": "description of module1",            "weeks": {"week1": {"title": "Week 01"}},"module2": {"title": "name of module2", "description": "description of module2",
+               // "weeks": { "week2": {"title": "Week 02"},"week3": { "title": "Week 03" } } } } }}')
+     
     
 }]);
