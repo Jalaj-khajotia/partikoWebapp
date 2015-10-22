b11d2ca6f66ea7c07d2b5e8ab7de94c0b96cf6f0
jalaj <jalajiitr@gmail.com>
2015-10-22 23:45:16 +0530

    Logout functionality

--- app/scripts/services/AuthenticationService.js
+++ app/scripts/services/AuthenticationService.js
@@ -4,29 +4,34 @@ angular.module('sbAdminApp')
 
 .factory('AuthenticationService',
     [ '$http','$window',
-    function ( $http, $window) {       
+    function ( $http, $window) {   
+
      var service = {};
-        service.Login = function (username, password, callback) {
 
-        var  body = '{"email" :"' + username +'", "password" :"'+ password +'" }'; 
+     service.Login = function (username, password, callback) {
+          var body = '{"email" :"' + username +'", "password" :"'+ password +'" }'; 
 
-         $http({method: 'POST', url: 'http://api.partiko.com/merchant/login',data:body}).
-        then(function(response) {
-          /*$scope.status = response.status;
-          $scope.data = response.data;*/
-          console.log( response.status);
-          if( response.data.status)
-          {
-             $window.location.href ="/#/dashboard/home";
-             sessionStorage.setItem('token', response.data.token);
-          }
-          else{
-            ;
-          }
-        }, function(response) {
-         alert("Either password or username is wrong");
-      });   
-
+          $http({method: 'POST', url: 'http://api.partiko.com/merchant/login',data:body})
+                .then(function(response) {
+                  /*$scope.status = response.status;
+                  $scope.data = response.data;*/
+                 // console.log( response.status);
+                  if( response.data.status)
+                  {
+                     $window.location.href ="/#/dashboard/home";
+                     sessionStorage.setItem('token', response.data.token);
+                  }
+                  else{            
+                  }
+                }, function(response) {
+                 alert("Either password or username is wrong");
+              });   
         };
+
+      service.Logout = function(){
+        sessionStorage.clear();
+         $window.location.href ="/#/login";
+      };
+
         return service;
     }]);
